3a250ae0b65b5f4c8b88722892e762d77b3ed55c
Saurav Prasad <sauravp@vmware.com>
2020-02-17 13:12:15 -0500

    Added testNG.xml configs into POM.xml

--- src/test/java/com/qa/tests/TestTrigger.java
+++ src/test/java/com/qa/tests/TestTrigger.java
@@ -1,5 +1,14 @@
 package com.qa.tests;
 
+import org.testng.annotations.Test;
+
 public class TestTrigger {
 
+	
+	@Test
+	public static void display()
+	{
+		System.out.println("good morning");
+	}
+	
 }
